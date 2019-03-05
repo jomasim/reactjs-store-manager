ddc6d08993994a385f1825bee64fc050d4cee989
jomasim <simjoms@gmail.com>
2019-03-05 17:29:48 +0300

    ft(home-page): design home page
    - install semantic react-ui
    - create home component with semantic ui
    
    [Finishes #164400098]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/index.jsx';
+import 'semantic-ui-css/semantic.min.css';
 class App extends Component{
 render () {
   return(
