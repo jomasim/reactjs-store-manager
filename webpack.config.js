bfebdf6c289b4184b0cb83bac54b109869d17ea8
jomasim <simjoms@gmail.com>
2019-03-05 17:29:48 +0300

    ft(home-page): design home page
    - install semantic react-ui
    - create home component with semantic ui
    
    [Finishes #164400098]

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,35 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       },
+      {
+        test: /\.(gif|ttf|png|woff|woff2|eot|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
+          ],
+      },
     ]
   },
   plugins: [
