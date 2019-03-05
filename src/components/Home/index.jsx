616b2b14f2bdf0c5052bedd54cd4ed5db521ca6a
jomasim <simjoms@gmail.com>
2019-03-05 17:29:48 +0300

    ft(home-page): design home page
    - install semantic react-ui
    - create home component with semantic ui
    
    [Finishes #164400098]

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,51 @@
 import React,{Component} from 'react';
+import './home.css'
+import {
+  Button,
+  Container,
+  Menu,
+  Segment,
+} from 'semantic-ui-react';
 
 class Home extends Component{
   render () {
-    return(
-      <h1>Hello Store Manager</h1>
+    return (
+          <Segment
+            inverted
+            textAlign='center'
+            className='main-segment'
+            vertical
+          >
+            <Menu
+              fixed
+              inverted
+              pointing
+              secondary
+              size='large'
+            >
+              <Container>
+                <Menu.Item as='a' active>
+                  Home
+                </Menu.Item>
+                <Menu.Item as='a'>Products</Menu.Item>
+                <Menu.Item as='a'>Sales</Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' primary>
+                    Log in
+                  </Button>
+                  <Button as='a' primary  className='sign-up-btn'>
+                    Sign Up
+                  </Button>
+                </Menu.Item>
+              </Container>
+            </Menu>
+            <Container>
+              <div className='main-title'>
+              <p>Store Manager</p>
+                <h2>Perfect solution for your store operations</h2>
+              </div>
+            </Container>
+          </Segment>
     )
   }
 
