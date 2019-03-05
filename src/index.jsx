35f4d5c10999ff6c7d8ce383de321cd524264a08
jomasim <simjoms@gmail.com>
2019-03-05 16:20:50 +0300

    ch(react-router-dom): set up react router dom
    - install react router dom
    - create home component
    - set up home route  with router dom
    
    [Finishes #164399503]

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css';
-
-const App = () =>{
-return <div><p>Hello</p></div>
-};
+import App from './App.jsx';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
